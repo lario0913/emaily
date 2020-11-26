2ba08dd9cba8c3caee64a10de5496b64adc82a1a
Lario <osunkunleolanrewaju@gmail.com>
2020-11-26 12:36:30 +0100

    keys removed

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,4 @@
 
-// 815865248046-f2cvrk4c2bt5b6bei1f0lesr8v0ivrpu.apps.googleusercontent.com
-// 8yJYWuIEeq8jqSOuZY76EI9g
-// "mongodb+srv://emaily-dev:<password>@emaily-dev.0gci4.mongodb.net/<dbname>?retryWrites=true&w=majority"
-
 if(process.env.NODE_ENV === 'production'){
     module.exports = require('./dev')
 }else{
